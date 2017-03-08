ea6f065fc401d3f6a5d7aefb9d54c3ed09777031
Evan <chaaoli@gmail.com>
2017-03-08 11:23:42 +0800

    update

--- redis/src/main/java/redis/Test.java
+++ redis/src/main/java/redis/Test.java
@@ -5,7 +5,11 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import baseFramework.exception.ServerException;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 
 /**
  * @author chao.li
@@ -16,8 +20,12 @@ public class Test {
 
 	@Autowired
 	private JedisCluster jedisCluster;
+	@Autowired
+	private JedisPool jedisPool;
+	@Autowired
+	private ShardedJedisPool shardedJedisPool;
 
-	public String getDevice(String key) {
+	public String read(String key) {
 
 		try {
 			return jedisCluster.get(key);
@@ -27,13 +35,41 @@ public class Test {
 		return null;
 	}
 
-	public String getDeviceWithEx(String key) {
+	public String readWithEx(String key) {
 
 		try {
 			return jedisCluster.get(key);
 		} catch (Exception e) {
 			throw new ServerException(e, "");
 		}
+	}
+
+	public String read1(String key) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			return jedis.get(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (jedis != null)
+				jedis.close();
+		}
+		return null;
+	}
+
+	public String read2(String key) {
+		ShardedJedis jedis = null;
+		try {
+			jedis = shardedJedisPool.getResource();
+			return jedis.get(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (jedis != null)
+				jedis.close();
+		}
+		return null;
 	}
 
 }
